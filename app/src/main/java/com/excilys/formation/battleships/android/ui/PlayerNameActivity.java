f4bcbea623e0efadd71bbb133d5f4b42b9467070
Portigliatti Thibault <thibault.portigliatti@ensta-paristech.fr>
2016-10-31 13:21:46 +0100

    step 1

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,15 +1,28 @@
 package com.excilys.formation.battleships.android.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import battleships.formation.excilys.com.battleships.R;
 
 public class PlayerNameActivity extends AppCompatActivity {
+    EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player_name);
+        setContentView(R.layout.activity_player_name);  // bind view
+        mNameEditText = (EditText) findViewById(R.id.example_text_name);  // bind widgets
+    }
+
+    public void onClickButton(View v) {
+        String name = mNameEditText.getText().toString();
+        if (!name.isEmpty()) {
+           /* do stuff */
+            Toast.makeText(PlayerNameActivity.this, name, Toast.LENGTH_LONG).show();
+        }
     }
 }
